57430ec3c1535cc88768924ea04e4d71c6984f94
Calvin Kwong <ckwong@us.costar.local>
2018-04-06 10:35:20 -0700

    adjust franchiseListings Component to display prop data

--- src/Components/Containers/franchiseListings.tsx
+++ src/Components/Containers/franchiseListings.tsx
@@ -20,41 +20,42 @@ class FranchiseListings extends React.Component<Props, State> {
         super(props);
     }
 
-    componentWillMount() {
+    componentDidMount() {
         this.setState({
             data: this.props.fetchFranchise()
         });
     }
 
     render() {
-        const franchiseListItem = this.props.franchise.map((item: Franchise) => {
+        const franchiseListItem = this.props.franchise.map((item: Franchise[]) => {
             const props = {
-                franchiseId: item.franchiseId,
-                name: item.name,
-                address: item.address,
-                address2: item.address2,
-                city: item.city,
-                stateId: item.stateId,
-                zip: item.zip,
-                countryId: item.countryId,
-                localPhone: item.localPhone,
-                minCapitalMin: item.minCapitalMin,
-                minCapitalMax: item.minCapitalMax,
-                minWorthMin: item.minWorthMin,
-                minWorthMax: item.minWorthMax,
-                franchiseFeeMin: item.franchiseFeeMin,
-                franchiseFeeMax: item.franchiseFeeMax,
-                totalInvestmentMin: item.totalInvestmentMin,
-                totalInvestmentMax: item.totalInvestmentMax,
-                lowCost: item.lowCost,
-                businessOpportunity: item.businessOpportunity,
-                highCapital: item.highCapital,
-                shortDescription: item.shortDescription,
-                generalDescription: item.generalDescription,
-                createdDate: item.createdDate,
-                franchiseImage: item.franchiseImage
+                franchiseId: item[0].franchiseId,
+                name: item[0].name,
+                address: item[0].address,
+                address2: item[0].address2,
+                city: item[0].city,
+                stateId: item[0].stateId,
+                zip: item[0].zip,
+                countryId: item[0].countryId,
+                localPhone: item[0].localPhone,
+                minCapitalMin: item[0].minCapitalMin,
+                minCapitalMax: item[0].minCapitalMax,
+                minWorthMin: item[0].minWorthMin,
+                minWorthMax: item[0].minWorthMax,
+                franchiseFeeMin: item[0].franchiseFeeMin,
+                franchiseFeeMax: item[0].franchiseFeeMax,
+                totalInvestmentMin: item[0].totalInvestmentMin,
+                totalInvestmentMax: item[0].totalInvestmentMax,
+                lowCost: item[0].lowCost,
+                businessOpportunity: item[0].businessOpportunity,
+                highCapital: item[0].highCapital,
+                shortDescription: item[0].shortDescription,
+                generalDescription: item[0].generalDescription,
+                createdDate: item[0].createdDate,
+                franchiseImage: item[0].franchiseImage
             };
-            return <FranchiseListItem key={item.franchiseId} {...props} />;
+            
+            return <FranchiseListItem key={item[0].franchiseId} {...props} />;
         });
 
         return(
