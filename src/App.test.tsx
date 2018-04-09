7ec6c80b2a28dc30dc80fabf848ad0d27e0679df
Calvin Kwong <ckwong@us.costar.local>
2018-04-09 16:28:34 -0700

    create test file using jest

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,19 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
 
+// testing
+import { Provider }  from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import * as ReduxPromise from 'redux-promise';
+import rootReducer from './Reducers/index';
+
+
+const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore); 
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(
+    <Provider store={createStoreWithMiddleware(rootReducer)}>
+      <App />
+    </Provider>, div);
 });
